d7880fa52550ad54d15a44acd9538af6ae063bfd
B45i <amalshajan2011@gmail.com>
2024-10-21 23:17:45 +0530

    device title.

--- src/components/DeviceInfoCard.tsx
+++ src/components/DeviceInfoCard.tsx
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getDevice } from "../services/device.service";
+import dayjs from "dayjs";
+import { IoHardwareChipOutline } from "react-icons/io5";
+import { FaClock, FaDroplet, FaTemperatureQuarter } from "react-icons/fa6";
+import { MdBattery2Bar } from "react-icons/md";
 
 export const DeviceInfoCard = ({ id }: { id: string }) => {
   const deviceInfoQuery = useQuery({
@@ -8,9 +12,47 @@ export const DeviceInfoCard = ({ id }: { id: string }) => {
     queryFn: () => getDevice(id),
   });
 
-  if (deviceInfoQuery.isLoading) {
-    return <div>Loading...</div>;
+  const device = deviceInfoQuery.data;
+  if (deviceInfoQuery.isLoading || !device) {
+    return <div className="col-span-2">Loading...</div>;
   }
 
-  return <div>DeviceInfoCard</div>;
+  return (
+    <div className=" bg-secondary  rounded-3xl overflow-hidden p-4 flex flex-col group">
+      <h3 className="text-2xl font-semibold text-white flex items-center gap-4">
+        <div className=" bg-primary text-black h-12 w-12 rounded-full grid place-content-center text-2xl">
+          <IoHardwareChipOutline />
+        </div>
+        <span>{device?.name}</span>
+      </h3>
+      <div className="grow overflow-auto mt-4 flex flex-col gap-2">
+        <div className="flex items-center gap-2">
+          <FaClock className="text-white" />
+          <span className="font-medium ">Last Seen: </span>
+          {dayjs(device.lastSeen).fromNow()}
+          <span className="text-xs">
+            ({dayjs(device.lastSeen).format("YYYY-MM-DD hh:mm A")})
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <FaTemperatureQuarter className="text-white" />
+          <span className="font-medium ">Last temperature: </span>
+          {device.temperature}°C
+        </div>
+
+        <div className="flex items-center gap-2">
+          <FaDroplet className="text-white" />
+          <span className="font-medium ">Last humidity: </span>
+          {device.humidity}%
+        </div>
+
+        <div className="flex items-center gap-2">
+          <MdBattery2Bar className="text-white" />
+          <span className="font-medium ">Battery: </span>
+          {device.battery}%
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/DeviceDashboard.tsx
+++ src/pages/DeviceDashboard.tsx
@@ -1,34 +1,33 @@
 import { useParams } from "react-router-dom";
-import { getDevice, maxMinStats } from "../services/device.service";
+// import { getDevice, maxMinStats } from "../services/device.service";
 import { DeviceInfoCard } from "../components/DeviceInfoCard";
 
 export const DeviceDashboard = () => {
   const { id } = useParams();
-  getDevice(id!);
 
-  const getData = async () => {
-    const x = [
-      () => maxMinStats(id!, "temperature", "lastYear", "min"),
-      () => maxMinStats(id!, "temperature", "lastYear", "max"),
-      () => maxMinStats(id!, "humidity", "lastYear", "max"),
-      () => maxMinStats(id!, "humidity", "lastYear", "min"),
-      () => maxMinStats(id!, "battery", "lastYear", "max"),
-      () => maxMinStats(id!, "battery", "lastYear", "min"),
-    ];
-    x.forEach(async (func) => {
-      try {
-        const data = await func();
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  };
+  // const getData = async () => {
+  //   const x = [
+  //     () => maxMinStats(id!, "temperature", "lastYear", "min"),
+  //     () => maxMinStats(id!, "temperature", "lastYear", "max"),
+  //     () => maxMinStats(id!, "humidity", "lastYear", "max"),
+  //     () => maxMinStats(id!, "humidity", "lastYear", "min"),
+  //     () => maxMinStats(id!, "battery", "lastYear", "max"),
+  //     () => maxMinStats(id!, "battery", "lastYear", "min"),
+  //   ];
+  //   x.forEach(async (func) => {
+  //     try {
+  //       const data = await func();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   });
+  // };
 
-  getData();
+  // getData();
 
   return (
-    <div>
+    <div className="grow w-full h-full relative auto-rows-[12rem] grid grid-cols-[repeat(auto-fill,minmax(22rem,1fr))] gap-4">
       <DeviceInfoCard id={id!} />
     </div>
   );
