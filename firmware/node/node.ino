c1399b98179637c5982713bae278aa0c539cc883
B45i <amalshajan2011@gmail.com>
2024-11-15 19:53:14 +0530

    Add LittleFS library and implement saving and loading MAC address to/from flash

--- firmware/node/node.ino
+++ firmware/node/node.ino
@@ -1,6 +1,7 @@
 // ESP8266 Node.
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <LittleFS.h>
 
 struct DataPacket {
   float temperature = 25.5;      // Placeholder temperature
@@ -10,20 +11,41 @@ struct DataPacket {
 };
 
 DataPacket dataPacket;
-uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };  // Broadcast address
+const char *macFile = "/hub_mac.conf";
 uint8_t esp32Mac[6];
 bool macReceived = false;
 
-// Callback when data is received
+
+void saveMacToFlash(const uint8_t *mac) {
+  File file = LittleFS.open(macFile, "w+");
+  if (file) {
+    file.write(mac, 6);
+    file.close();
+  }
+}
+
+bool loadMacFromFlash(uint8_t *mac) {
+  if (!LittleFS.exists(macFile)) {
+    return false;
+  }
+
+  File file = LittleFS.open(macFile, "r");
+  if (!file) {
+    return false;
+  }
+
+  file.read(mac, 6);
+  file.close();
+  return true;
+}
+
 void onDataReceive(uint8_t *macAddr, uint8_t *incomingData, uint8_t len) {
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-           macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
-  Serial.print(macStr);
-  if (len == 6) {  // Check if the payload contains a MAC address
+  Serial.println("Data received");
+  if (len == 6) {
     memcpy(esp32Mac, incomingData, 6);
     macReceived = true;
     Serial.println("MAC address received from ESP32:");
+    saveMacToFlash(esp32Mac);
     for (int i = 0; i < 6; i++) {
       Serial.printf("%02X", esp32Mac[i]);
       if (i < 5) Serial.print(":");
@@ -32,35 +54,55 @@ void onDataReceive(uint8_t *macAddr, uint8_t *incomingData, uint8_t len) {
   }
 }
 
-// Callback when data is sent
 void onDataSent(uint8_t *macAddr, uint8_t sendStatus) {
   Serial.println(sendStatus == 0 ? "Broadcast sent successfully" : "Broadcast failed");
+  // todo: Intiate re-pairing after few tries, make inbuilt glow if data failed.
 }
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-
+void setupEspNow() {
+  Serial.println("Setting up ESP-NOW");
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_recv_cb(onDataReceive);
   esp_now_register_send_cb(onDataSent);
+  Serial.println("ESP-NOW ready");
+}
 
-  // Add the broadcast peer
+void sendMacRequestBroadcast() {
+  Serial.println("Sending broadcast to request MAC address...");
+  uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };  // Broadcast address
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+  esp_now_send(broadcastAddress, (uint8_t *)&dataPacket, sizeof(dataPacket));
+}
+
+void sendDataToHub() {
+  Serial.println("Sending data to hub...");
+  esp_now_send(esp32Mac, (uint8_t *)&dataPacket, sizeof(dataPacket));
+}
+
+void setup() {
+  Serial.begin(115200);
+  LittleFS.begin();
+  WiFi.mode(WIFI_STA);
+  setupEspNow();
+
+  if (loadMacFromFlash(esp32Mac)) {
+    Serial.println("Loaded MAC from flash.");
+    macReceived = true;
+    // addHubAsPeer();
+  }
 }
 
 void loop() {
   if (!macReceived) {
-    Serial.println("Sending broadcast to request MAC address...");
-    esp_now_send(broadcastAddress, (uint8_t *)&dataPacket, sizeof(dataPacket));
+    sendMacRequestBroadcast();
     delay(2000);  // Retry every 2 seconds
   } else {
     Serial.println("MAC address obtained. Ready for direct communication.");
     delay(5000);  // Pause after obtaining the MAC
+    sendDataToHub();
   }
 }
